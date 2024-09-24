b3c335ee9b979ce25a8586bbf643a16f44ca2b43
Jhostin Posada Cano <jhostinposada7@gmail.com>
2024-09-24 16:37:00 -0500

    Se creo un metodo para saber si un usuario ya existe con la informacion que le pasa el nuevo usuario atraves de los inputs

--- Registo_Inicio/models.py
+++ Registo_Inicio/models.py
@@ -2,14 +2,27 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Cada modelo representa una tabla, cada vez que se instancia un modelo este
+
+
+# Entidad que representa la publicacion de una inmobiliaria.
 class PublicacionInmobiliaria(models.Model):
+    # Atributo titulo
     titulo = models.CharField(max_length=100)
+    # Atributo  fecha
     fecha = models.DateField(default="2005/08/09") 
+    # Atributo precio
     precio = models.IntegerField(default=0)
+    # Atributo descripcion
     descripcion = models.TextField(default=0)
+    # Atributo imagens
     imagenes = models.ImageField(blank=True)
+    # Atributo usuario
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE, related_name= "Publicacion_guardada_Usuario", default= None)
     # prueba = models.CharField(max_length=100)
 
+    # Metodo magico para mostrar el titulo
     def __str__(self) -> str:
         return self.titulo
     
--- Registo_Inicio/views.py
+++ Registo_Inicio/views.py
@@ -1,16 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from django.http import HttpResponse
+# from django.db import connection
+from django.db.models import Q
 # Create your views here.
+# Vista para registrar el usuario
 def registroUsuario(request):
+    # Condicional para saber con que metodo esta interactuando el usuario
     if request.method == "GET":
+        # Carga el html si el usuario ingresa atraves del metodo get
         return render(request,"registro.html")
     else:
+        # Mira si la contraseña concuerda con la contraseña dos(Contraseña Confirmacion)
+
+        # Si coincide es porque se puede crear el usuario
         if request.POST["password"] == request.POST["passwordDos"]:
-            usuario = User.objects.create_user(username=request.POST["username"],email= request.POST["email"], password= request.POST["password"], first_name = request.POST["first_name"], last_name = request.POST["last_name"])
-            usuario.save()
-            print("Usuario creado con exito")
-            return HttpResponse("Usuario creado con exito")
+         
+            # Se crear un queryset con los datos que coincidan con los datos enviados por el usuario atraves del metodo post
+            usuarios = User.objects.filter(Q( username = request.POST["username"]) | Q(email = request.POST["email"]))
+
+            # Si hay usuarios con los mismos datos que el usuario ingreso
+            if usuarios.exists():
+
+                print(usuarios)
+                # Le genera un html con el siguiente mensaje
+                return HttpResponse("El usuario ya existe")
+            # Si no existe ninguno se crea un nuevo usuario
+            else:
+                
+                # Se crea un nuevo usuario
+                nuevoUsuario = User.objects.create_user(username= request.POST["username"], 
+                email = request.POST["email"], password = request.POST["password"], first_name = request.POST["first_name"], last_name = request.POST["last_name"])
+                #
+                print(usuarios)
+                # Se muestra el html con un mensaje de usuario creado con exito
+                return HttpResponse("Usuario creado con exito")
+
         else:
+            # 
             print("El usuario no fue creado con exito")
             return HttpResponse("El usuario no pudo ser creado")
+        
+
+def inicioSesionUsuario(request):
+    pass
