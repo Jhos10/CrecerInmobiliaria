1361be8400963fd6c857bfa83aac8f3bdc8fb4ae
Jhostin Posada Cano <jhostinposada7@gmail.com>
2024-11-11 10:48:11 -0500

    Ultimo commit todo va bien con el proyecto

--- otrasSecciones/urls.py
+++ otrasSecciones/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
- path('SobreNosotros/', views.inmobiliaria_view, name = 'sobreNosotros')
+ path('SobreNosotros/', views.inmobiliaria_view, name = 'sobreNosotros'),
+ path('contactanos/', views.contactanos, name='contactenos')
 ]
